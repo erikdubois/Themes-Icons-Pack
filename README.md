59b34cbbe996fb647727e6020c586377162b0dd2
erikdubois <erik.dubois@gmail.com>
2015-05-17 11:42:06 +0200

    Automatic Backup @ Sun May 17 11:42:06 CEST 2015

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # themes-icons-pack
+
+Check if you have git installed else
+
+	sudo apt-get install git
+
+Now you can use the command git clone in the terminal and run scripts.
+
+Beware themes will be copied to ~/.themes
+
+Beware icons will be copied to ~/.icons
+
+Beware cursors will be copied to ~/.themes
+
+
+It is up to you to activate them.
+
+
+#1. Ardis icon theme
+
+https://github.com/KotusWorks/Ardis-icon-theme
---
+++ installation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+#======================================================================================
+#                               themes-icons-pack
+# 
+# Author  : Erik Dubois at http://www.erikdubois.be
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
+#======================================================================================
+
+
+
+
+########################################
+########        T H E M E S    #########
+########################################
+
+git clone https://github.com/KotusWorks/Ardis-icon-theme.git ~/.themes/Ardis-icon-theme
+
+
+
+
+
+
+
+
+
+########################################
+########        I C O N S      #########
+########################################
+
+
+
+
+
+
+
+
+
