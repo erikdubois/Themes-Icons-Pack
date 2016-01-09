64a6d4494d5b06c9aee49a15c535e2c1df2fce7a
erikdubois <erik.dubois@gmail.com>
2016-01-09 20:11:31 +0100

    Automatic Backup @ Sat Jan  9 20:11:31 CET 2016

---
+++ installation_ultra_flat_orange.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+#======================================================================================
+#                               themes-icons-pack
+# 
+# Author  : Erik Dubois at http://www.erikdubois.be
+# License : Distributed under the terms of GNU GPL version 2 or later
+# 
+# AS ALLWAYS, KNOW WHAT YOU ARE DOING.
+#======================================================================================
+
+
+########################################
+########        T H E M E S    #########
+########################################
+
+
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install ultra-flat-icons
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
